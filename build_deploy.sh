550ae39b12c0f2a664745f04330e031ae1b0f34c
Vihar Devalla <devalla@arcesium.com>
2023-01-25 13:07:26 +0530

    Function based code in build script

--- build_deploy.sh
+++ build_deploy.sh
@@ -1,56 +1,71 @@
 
-python3 docker-build/server.py --test &
-PID=$!
-python3 docker-build/test.py
+#!/bin/bash
 
-if [ $? -eq 0 ];
-then
+run_tests(){
+    python3 docker-build/server.py --test &
+    PID=$!
+    python3 docker-build/test.py
+}
+
+start_minikube(){
     kill $PID
     unset KUBECONFIG
     MINICHECK=$(minikube status | grep kubelet | awk '{ print $2}')
-    echo $MINICHECK
+    echo "Minikube Cluster is " $MINICHECK
     if [ $MINICHECK != "Running" ];
     then
         minikube start
         minikube addons enable ingress
     fi
+}
 
-
+build_docker(){
     eval $(minikube -p minikube docker-env)
 
-
     echo "Docker Build"
-    echo -e
     docker build -t vihar/athlete-server docker-build/
-    echo -e
 
-
-    echo -e
     echo "Docker Size"
     echo $(docker images | grep SIZE)
     echo $(docker images | grep vihar/athlete-server)
-    echo -e
+}
 
-    echo -e
+start_cluster(){
     kubectl apply -f manifests/deployment.yaml
     kubectl apply -f manifests/service.yaml
     kubectl delete -A ValidatingWebhookConfiguration ingress-nginx-admission
     kubectl apply -f manifests/ingress.yaml
-    echo -e
+}
 
-    echo -e
+wait_for_deploy(){
     echo "Waiting for Server to deploy"
     sleep 10
+}
 
-    echo -e
-    echo -e
+
+test_endpoint(){
     echo "Testing the IP endpoint - 192.168.49.2/athlete"
     curl $(minikube ip)/athlete
-    echo -e
+
     echo "Testing the nip.io endpoint - c0a83102.nip.io/athlete"
     curl c0a83102.nip.io/athlete
-    echo -e
-    echo -e
+}
+
+
+run_tests
+
+if [ $? -eq 0 ];
+then
+    echo "Starting Minikube"
+    start_minikube
+
+    build_docker
+
+    start_cluster
+
+    wait_for_deploy
+
+    test_endpoint
 
 else
     kill $PID
